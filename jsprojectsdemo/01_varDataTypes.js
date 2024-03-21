d123feaa72847fac46d68df240f27ec12c67ae11
Rrdsrd <113920658+Rrdsrd@users.noreply.github.com>
2024-03-21 17:39:30 +0000

    conversionDatatypes

---
+++ jsprojectsdemo/01_varDataTypes.js
@@ -0,0 +1,14 @@
+const accountId=123456
+let accountEmail="charmy.smrutidash1@gmail.com"
+accountCity="Ahamadabad"
+let accountState;
+console.log(accountId)
+console.table([accountEmail,accountCity,accountState])
+//DataTypes
+// number
+// BigInt
+// String
+// Boolean
+// null
+// Symbol
+// undefined
