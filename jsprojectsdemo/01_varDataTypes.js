fee7d936e005950c9f6c357253bc113a1960732a
Rrdsrd <113920658+Rrdsrd@users.noreply.github.com>
2024-03-22 14:41:09 +0000

    added some notes

--- jsprojectsdemo/01_varDataTypes.js
+++ jsprojectsdemo/01_varDataTypes.js
@@ -12,3 +12,39 @@ console.table([accountEmail,accountCity,accountState])
 // null
 // Symbol
 // undefined
+//There are total 2 types of datatypes avilable in Javascript
+//Primitive 
+//String  
+ const name="smruti"
+//Number 
+const mark=100      
+const mark3=100.3
+//Boolean    
+const isvalid=true   
+//null
+const mark1=null
+//undefined
+let mark2;
+//symbol
+const emp_id=Symbol('123')
+const emp_id1=Symbol('123')
+console.log(emp_id==emp_id1)
+//bigint
+const largeno=236783245679n
+//nonPrimitives
+//Arrays 
+const courses=['Java','python','c','javascript']
+//Objects
+const course_fees=
+{
+    'Java':5000,
+    'python':7000,
+    'c':4000,
+    'javascript':4000,
+}
+//functions
+const my_func=function()
+{
+    console.log("hello world")
+}
+console.log(typeof(largeno))
