9d6f4c9bb6026182ae1a31dc0bb3372582b25c87
Smruti Rekha Dash <113920658+Rrdsrd@users.noreply.github.com>
2024-03-28 10:39:24 +0000

    IIFE done again

--- jsprojectsdemo/10.IIFE.js
+++ jsprojectsdemo/10.IIFE.js
@@ -1,1 +1,17 @@
 //Imidiately Invoked Function Expression(IIFE)
+//to avoid global pollution
+//nameD IIFE
+(function add()
+{
+    console.log("Db Connection done");
+})();
+//nameD IIFE
+(function addagain()
+{
+    console.log("Db Connection done again");
+})();
+//unnameD IIFE
+((name)=>
+{
+    console.log(`${name}`);
+})('smruti')
