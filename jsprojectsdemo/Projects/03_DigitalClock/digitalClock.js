599cd4cefb9fbef5ba35c92760d0175338637b36
Smruti Rekha Dash <113920658+Rrdsrd@users.noreply.github.com>
2024-04-01 16:35:56 +0000

    Project3 done

---
+++ jsprojectsdemo/Projects/03_DigitalClock/digitalClock.js
@@ -0,0 +1,6 @@
+const clock=document.getElementById("clock")
+//console.log(date.toLocaleTimeString())
+setInterval(function(){
+    let date=new Date()
+    clock.innerHTML=date.toLocaleTimeString()
+},1000);
